0ef9f00cc846d3f6b0fbcb81d29343f2aec7af47
rohitjangid01 <rohit.devteam@webdaire.com>
2025-07-16 15:26:15 +0530

    setup db config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from app.api import auth
+from app.database import Base, engine
 
 app = FastAPI()
 
+# Auto-create tables
+Base.metadata.create_all(bind=engine)
+
+# Routes
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+# app.include_router(products.router, prefix="/products", tags=["Products"])
+# app.include_router(cart.router, prefix="/cart", tags=["Cart"])
+
 @app.get("/")
-def read_root():
-    return {"status": "API is running smoothly"}
+def root():
+    return {"success": True, "message": "Apni Dukaan backend is working!"}
